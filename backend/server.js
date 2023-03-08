2b8eac745d03a73f075e028a6453859c97722ae7
Neilly <neilpilarca@gmail.com>
2023-03-08 10:52:11 +0100

    add server fix

--- backend/server.js
+++ backend/server.js
@@ -29,13 +29,20 @@ app.use("/api/user", userRoutes);
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalle", dalleRoutes);
 
+// message for deployment
+app.get("/", async (req, res) => {
+  res.status(200).json({
+    message: "Hello from Pokehack!",
+  });
+});
+
 // connect to db
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
     // listen for requests
-    app.listen(process.env.PORT, () => {
-      console.log("connected to db & listening from port", process.env.PORT);
+    app.listen(4000, () => {
+      console.log("connected to db & listening from port 4000");
     });
   })
   .catch((error) => {
