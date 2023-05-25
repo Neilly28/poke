538cf00c14fc8de1b985c6fe68864189149cf24b
Neilly28 <neilpilarca@gmail.com>
2023-05-25 14:48:51 +0200

    add navbar fix

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,10 +5,11 @@ import { AuthContext } from "../context/AuthContext";
 
 const Navbar = () => {
   const { user, handleLogout } = useContext(AuthContext);
-  const [showMenu, setShowMenu] = useState(false);
 
   // header state
   const [isActive, setIsActive] = useState(false);
+
+  const [isSmallerDevice, setIsSmallerDevice] = useState(false);
 
   // event listener
   useEffect(() => {
@@ -17,14 +18,19 @@ const Navbar = () => {
     });
   });
 
-  const handleClick = () => {
-    handleLogout();
-    setShowMenu(false);
-  };
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallerDevice(window.innerWidth <= 1014);
+    };
 
-  const handleMenu = () => {
-    setShowMenu(!showMenu);
-  };
+    handleResize(); // Initial check on component mount
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   // Check if the current location path is "/login"
   const location = useLocation();
@@ -40,17 +46,19 @@ const Navbar = () => {
   return (
     <div>
       <header
-        className={`flex items-center justify-around p-12 fixed top-0 w-full z-10 transition-all ${
+        className={`flex justify-center lg:justify-between items-center p-12 fixed top-0 w-full z-10 transition-all ${
           isActive
             ? "bg-black py-4 shadow-md text-white"
             : "bg-[#F7D02C] py-6 text-black"
         }`}
       >
-        <Link to="/home">
-          <h1 className="text-2xl font-bold">{"<Pokéhack />"}</h1>
-        </Link>
-        <div className="hidden sm:flex">
-          <div className="flex items-center font-semibold gap-4">
+        {!isSmallerDevice && (
+          <Link to="/home">
+            <h1 className="text-2xl font-bold">{"<Pokéhack />"}</h1>
+          </Link>
+        )}
+        <div>
+          <div className="flex max-w-lg mx-auto items-center font-semibold gap-4 justify-center sm:justify-between">
             <Link
               to="/create"
               className="flex justify-center items-center px-4 py-2 capitalize text-sm font-bold text-white rounded-3xl bg-red-500 hover:bg-red-600"
@@ -63,9 +71,7 @@ const Navbar = () => {
 
             {!user && (
               <>
-                <span className="hidden sm:block text-sm font-bold">
-                  Hello, Guest
-                </span>
+                <span className="text-sm font-bold">Hello, Guest</span>
                 <Link className="text-sm font-bold" to="/login">
                   Login
                 </Link>
@@ -77,83 +83,14 @@ const Navbar = () => {
                 <span className="hidden sm:block text-sm font-bold">
                   Hello, {user.email.slice(0, user.email.indexOf("@"))}
                 </span>
-                <button className="text-sm font-bold" onClick={handleClick}>
+                <button className="text-sm font-bold" onClick={handleLogout}>
                   Log Out
                 </button>
               </>
             )}
           </div>
         </div>
-        {/* mobile button goes here */}
-        <div className="flex sm:hidden items-center">
-          <button className="mobile-menu-button" onClick={handleMenu}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-12 h-12"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-              />
-            </svg>
-          </button>
-        </div>
-        {/* mobile menu goes here */}
       </header>
-      <div className={`${showMenu ? "" : "hidden"}`}>
-        <Link
-          to="/home"
-          className="block p-4 text-2xl bg-red-500 text-white hover:bg-red-600"
-          onClick={() => {
-            setShowMenu(false);
-          }}
-        >
-          Home
-        </Link>
-        <Link
-          to="/create"
-          className="block p-4 text-2xl bg-red-500 text-white hover:bg-red-600"
-          onClick={() => {
-            setShowMenu(false);
-          }}
-        >
-          Create
-        </Link>
-
-        <Link
-          to="/community"
-          className="block p-4 text-2xl bg-red-500 text-white hover:bg-red-600"
-          onClick={() => {
-            setShowMenu(false);
-          }}
-        >
-          Community
-        </Link>
-
-        {user && (
-          <button
-            onClick={handleClick}
-            className="block w-full text-left p-4 text-2xl bg-red-500 text-white hover:bg-red-600"
-          >
-            Logout
-          </button>
-        )}
-        {!user && (
-          <Link to="/login">
-            <button
-              onClick={handleClick}
-              className="block w-full text-left p-4 text-2xl bg-red-500 text-white hover:bg-red-600"
-            >
-              Login
-            </button>
-          </Link>
-        )}
-      </div>
     </div>
   );
 };
